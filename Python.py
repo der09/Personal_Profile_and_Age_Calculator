f8b096c4e54e0b58762b2796e78c308c8b1f1eea
StudentAM <studentam@Administrators-MacBook-Pro.local>
2026-08-26 10:29:24 -0700

    worked on it

--- Python.py
+++ Python.py
@@ -1,15 +1,24 @@
 # Personal_Profile_and_Age_Calculator
 
+
+print()
+print()
+
 #the introduction 
-print("Hello! Welcome!!)
+print("Hello! Welcome!!")
 print("I will be asking some questions!")
 print("Then I'll calculate how long you've lived!")
+
+print()
 
 #asking for the users name
 name = input("What's your name?: ")
 
 #asks user for birth year then converts into integer 
-birth_year = int(input("What year were you born?: "))
+birth_year = input("What year were you born? ")
+birth_year_num = int(birth_year)
+
+print() 
 
 #asking for their favorites 
 favorite_food = input("What's your favorite food?: ")
@@ -23,21 +32,27 @@ favorite_season = input("What's your favorite season?: ")
 favorite_game = input("What's your favorite game?: ")
 
 #the calculations for how long the user has lived in years 
-age = 2026 - birth_year
+age = 2026 - birth_year_num
 
 #calculations for how long they've lived specifically 
 months_alive = age * 12
 days_alive = age * 365
 hours_alive = days_alive * 24 
 minutes_alive = hours_alive * 60
-seconds_alive = hours_alive * 60 
+seconds_alive = minutes_alive * 60 
 
+print() 
 #the beginning of the personalized profile (the final product)
 print("This is your profile!")
+
+print()
+print()
 
 print("Hello! " + name)
 print("You were born in the year " + birth_year)
 print("You live in the city " + city)
+
+print()
 
 #telling the user their favorites 
 print("Your favorite food is " + favorite_food)
@@ -45,13 +60,21 @@ print("Your favorite hobby is " + favorite_hobby)
 print("Your favorite season is " + favorite_season)
 print("Lastly, your favorite game is " + favorite_game)
 
+
+print()
+print()
+
 #telling the user how long they've lived 
-print("Your age is " + age)
-print("You've lived " + months_alive + " months!")
-print("You've lived " + days_alive + " days!")
-print("You've lived " + hours_alive + " hours!")
-print("You've lived " + minutes_alive + " minutes!") 
-print("You've lived " + seconds_alive + " seconds!")
+print("Your age is ", age)
+print("You've lived ", months_alive, " months!")
+print("You've lived ", days_alive,  " days!")
+print("You've lived ",  hours_alive, " hours!")
+print("You've lived ", minutes_alive, " minutes!") 
+print("You've lived ",  seconds_alive,  " seconds!")
+
+
+print() 
+print() 
 
 #the closing statements 
 print("Thank you so much for participating!")
